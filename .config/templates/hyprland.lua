525c410f2d329e1a6e41a8b798a8dbebc4efe524
Radosław Kaczmarczyk <radoslaw.kaczmarczyk@proton.me>
2026-09-05 18:01:29 +0200

    remove templates

--- .config/templates/hyprland.lua
+++
@@ -1,6 +0,0 @@
-return {
-  primary = "rgb({{ colors.primary.default.hex_stripped }})",
-  secondary = "rgb({{ colors.secondary.default.hex_stripped }})",
-  surface_variant = "rgba({{ colors.surface_variant.default.hex_stripped }}aa)",
-  background = "rgb({{ colors.background.default.hex_stripped }})"
-}
