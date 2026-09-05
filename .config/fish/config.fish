3cbbf9b5683b7d23a55c4a245004aa975fb0bf27
Radosław Kaczmarczyk <radoslaw.kaczmarczyk@proton.me>
2026-09-05 14:21:50 +0200

    update fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -14,6 +14,15 @@ alias cls="clear"
 
 set -gx EDITOR nvim
 
-fish_add_path ~/.local/bin
+fish_add_path $HOME/.local/bin
+fish_add_path (go env GOPATH)/bin
+fish_add_path /usr/local/go/bin
+fish_add_path $HOME/.cargo/bin
+fish_add_path $HOME/.local/share/pnpm/bin
+fish_add_path $PNPM_HOME
+fish_add_path $BUN_INSTALL/bin
+fish_add_path $HOME/.spicetify
+fish_add_path $HOME/.opencode/bin
+fish_add_path $HOME/.config/scripts
 
 zoxide init fish | source
